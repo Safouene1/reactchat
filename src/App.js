a47349e229ddcd54992fd2ee49c1c0192b266152
Safouene Turki <tsafouen@gmail.com>
2021-02-20 20:39:02 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {ChatEngine} from 'react-chat-engine';
+import './App.css'
+import ChatFeed from './components/ChatFeed.jsx' 
+const App = () => {
+  return ( 
+    <ChatEngine 
+    height="100vh"
+    projectID="b84c546f-5095-4d5a-b514-fc3adbc9ed28"
+    userName="safouen"
+    userSecret="1212"
+    renderChatFeed={ (chatAppProps) => <ChatFeed {...chatAppProps} /> }
+    />
+   );
 }
-
+ 
 export default App;
